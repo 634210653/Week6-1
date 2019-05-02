bc1f88ec04742a65d94d50a0dac8550641dd64bc
LP_PCPC_Master <634210653@qq.com>
2019-05-02 17:57:22 +0800

    finish practice2-practice4

--- practice2.sql
+++ practice2.sql
@@ -1,2 +1,3 @@
-# 取得每个company中最高薪水的人员名字
+﻿# 取得每个company中最高薪水的人员名字
 # 输出结果包含公司名称和人员名称：companyName name
+select  companyName ,name from employee as em  inner join company as com on em.companyId = com.id  order by salary desc limit 1 ;
--- practice3.sql
+++ practice3.sql
@@ -1,4 +1,6 @@
-#取得平均薪水最高的公司
+﻿#取得平均薪水最高的公司
 #输出公司名称和平均薪水：companyName avgSalary
 
-
+select companyName,avg(salary) as avgSalary from employee as em  inner join company as com on em.companyId = com.id
+group by companyId
+order by avgSalary desc limit 1;
